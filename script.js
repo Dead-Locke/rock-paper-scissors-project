e67644a8d5cba56b8a63b84684aa3375a64c5d3f
Dead-Locke <impulse25@ymail.com>
2022-02-21 19:53:33 -0500

    Finalized most of the site -  Added all elements. Player health, Opponent move display, etc. - Added endgame display and animations, including fade in and typing effects. - Adjusted timing for all animations.

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-const weapons = document.querySelectorAll('.weapon');
+const elementsArea = document.querySelector('.elemnt-area');
+const elements = document.querySelectorAll('.element');
 const rounds = document.querySelector('.round');
+const message = document.querySelector('.message');
+const computerImage = document.querySelector('.computer-image');
+const playerHearts = document.querySelector('#pHearts')
+const computerHearts = document.getElementById('cHearts');
+const statusMessage = document.querySelector('.status-message');
+const statusIcon = document.querySelector('.status-icon');
+const playAgain = document.querySelector('.play-again');
+var round = 0; 
 
-
-
-
-
-
+var msg = { //win/lose/tie messages to display;
+    fireWin: "Scorch their lands, burn down the forests!",
+    fireLose: "Your pathetic flame sparks and flickers away...",
+    fireTie: "Its a tie! The world burns, but you live to see another day",
+    waterWin: "Drown out their cries, flood their basements, extinguish their flames!" ,
+    waterLose: "Grass absorbs your water, you lose..." ,
+    waterTie: "Its a tie! The seas rise, and you both rise with them!",
+    grassWin: "Quench your thirst, grow stronger! Congratulations!",
+    grassLose: "Your roots singe and burns away...Try again.",
+    grassTie: "The sun shines bright! You grow stronger...together!",
+};
 
 function computerMove(){ // randomly roll fire, water or grass.
     var roll = randomIntInterval(1,3)
@@ -16,74 +31,109 @@ function randomIntInterval(min, max){ // returns random integer in a range
     return Math.floor(Math.random()*(max-min+1) + min)
 }
 
-function play() { //given two inputs, decides the winner 
-    let win = false; 
-
-    weapons.forEach(weapon => {
-        weapon.addEventListener('click', () => {
-            
-            const weaponIcons = document.querySelectorAll('.w-icon')
-            let c = computerMove();
-            let p; 
-            
-
-            if(weapon.classList.contains('fire')){
-                 p = 'fire';
-                 weaponIcons[0].filter = none;
-                 weaponIcons[1].filter = grayscale;
-                 weaponIcons[2].filter = grayscale;
-            };
-            if(weapon.classList.contains('water')){
-                p = 'water';
-                weaponIcons[1].filter = none;
-            };
-            if(weapon.classList.contains('grass')){
-            p = 'grass';
-            weaponIcons[2].filter = none;
-            };
-            
-            playRound(c, p)
-
-
-
-        })
-
-
-    })
-}
-function playRound(p,c){ 
-    // win conditions
-    if(p.toLowerCase() == 'fire') if(c== 'grass') win = true; 
-    if(p.toLowerCase() == 'water')  if(c== 'fire') win = true;
-    if(p.toLowerCase() == 'grass') if(c== 'water') win = true;
-
-    return p.toLowerCase() == c.toLowerCase() ? "Its a Tie!": // print out tie, win or lose depending on 'win' variable.
-        win ? `You Win! ${p} beats ${c}` : `You Lose! ${c} beats ${p}`;
-
+//the following 3 functions receive the computer move and a win/lose/tie  message, display both, then increment the hearts/lives and rounds
+function win(computer, msg){ 
+    computerHearts.removeChild(computerHearts.lastChild);
+    message.innerText = msg;
+    message.style.color = 'green';
+    computerImage.src = `images/${computer}.png`;
+    rounds.innerText = `Round: ${++round}`;  
 }
 
-function game(goal){ // plays the game, first to goal, asks the player for input and plays against a computer, printing results of each round.
-    var playerScore = 0; var computerScore = 0;
-    var i = 1
-    while(playerScore < goal && computerScore < goal){ // loop until player or computer get 5 points.
-        let p = prompt("Please enter fire, water, or grass"); //store moves as variables to print out
-        let c = computerMove(); 
-        let result = playRound(p, c)[4] //store 5th letter W, L or a to determine winner or tie.
+function lose(computer, msg){
+    playerHearts.removeChild(playerHearts.lastChild);
+    message.innerText = msg;
+    message.style.color = 'red'; 
+    computerImage.src = `images/${computer}.png`;
+    rounds.innerText = `Round: ${++round}`; 
+}
 
-        if(result == 'W') playerScore++; // distribute scores
-        if(result == 'L') computerScore++;
-        i++; // increment round
+function tie(choice, msg){
+    message.innerText = msg;
+    message.style.color = 'black'; 
+    computerImage.src = `images/${choice}.png`;
+    rounds.innerText = `Round: ${++round}`; 
+}
 
-        console.log(`Round ${i}: Player: ${p} --- Computer: ${c}   Score: ${playerScore}-${computerScore}`); //print last moves and total score 
+function playRound(p,c){ //given inputs player and comp, find winner & display msg & icon
+    if(p == 'fire'){ 
+        if(c== 'grass') win('grass', msg.fireWin)
+        else if(c== 'water') lose('water', msg.fireLose) 
+        else tie('fire', msg.fireTie)
+    }
+    if(p == 'water') { 
+        if(c== 'fire') win('fire', msg.waterWin)
+        else if(c== 'grass') lose('grass', msg.waterLose) 
+        else tie('water', msg.fireTie)
+    }
+    if(p == 'grass'){
+        if(c== 'water') win( 'water', msg.grassWin);  
+        else if(c== 'fire') lose('fire', msg.grassLose);
+        else tie('grass', msg.grassTie);
+    } 
+}
+
+function end(){ //end game status display
+    if(computerHearts.childElementCount == 0){ //end game message if you win
+        document.querySelector('.element-area').remove();
+        setTimeout(()=> {
+            endDisplay('WINNER!', 'green', 'win', 'Play Again?');
+         }, 1000);
+         setTimeout(()=> {
+            playAgain.classList.add('playEnable'); //enable hover effect on button
+        }, 7000);
     }
 
-    return playerScore > computerScore ? `${playerScore}-${computerScore} You Win!` : 
-            playerScore < computerScore ? `${playerScore}-${computerScore} You Lose!` : `${playerScore}-${computerScore} It's a tie!`
+    if(playerHearts.childElementCount == 0){ //end game message if you lose
+        document.querySelector('.element-area').remove();
+        setTimeout(()=> {
+            endDisplay('LOSER! ', 'red', 'lose', 'Try Again?');
+        }, 1000);
+        setTimeout(()=> {
+            playAgain.classList.add('playEnable');
+        }, 7000);
+    }
 }
 
-play();
+function endDisplay(text, color, img, buttonText){ //changes end status based on winner
+    document.querySelector('.endgame').style.height = 'auto';
+    document.querySelector('.endgame').style.visibility = 'visible';
+    statusMessage.innerText = text;
+    statusMessage.style.color = color;
+    document.querySelector('.status-message').classList.add('statusTyped');//winner or loser status
+    statusIcon.src = `images/${img}.png`; //ghost or trophy img
+    document.querySelector('.status-icon').classList.add('appearIcon'); //animation for fadein
+    playAgain.innerText = buttonText; //change button text to 'Play Again' or 'Try Again'
+    document.querySelector('.play-again').classList.add('appearPlayAgain'); //animation fade in
+    document.querySelector('.thankyou').classList.add('thanksTyped'); //animation for thank you message
 
-const weapons = document.querySelectorAll('weapon')
+}
+
+function play() { //listen for button clicks and play round each click
+    elements.forEach(elem => {
+        elem.addEventListener('click', () => {
+            elements.forEach(x => x.classList.remove('highlight')); //remove highlight
+            let c = computerMove(); // rolls computers move
+            let p;  // players move or choice depending on which button they click
+            if(elem.classList.contains('fire')) p = 'fire';
+            if(elem.classList.contains('water')) p = 'water';
+            if(elem.classList.contains('grass')) p = 'grass'; 
+
+            elem.className += ' highlight'; //player choice highlighted
+
+            playRound(p,c); //play a round and adjust score, display all appropriate info
+            end(); // check for end of game and display play again message
+
+        });
+    });
+
+    playAgain.onclick = () => window.location.reload();
+
+}
+
+
+
+play();
 
 
 
